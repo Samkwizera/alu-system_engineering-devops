43617dfef4a7b93d1fd8db498427578667dd2a03
root <s.ihimbazwe@alustdudent.com>
2025-04-03 13:58:40 +0000

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
-# Start Apache so as to return a
-# page conatining Hello Holberton
-service apache2 start
+# fix nginx to accept and serve more requests
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
